f38a764d3911377f8465c560025a66acb2372dda
pavan <130090432+pavan-kolagani@users.noreply.github.com>
2025-05-16 16:54:25 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-
-from pathlib import Path
-from shiny import App
-from ui_components import main_ui
-from handlers import server
-
-
-data_dir = Path(__file__).parent / "data"
-app = App(ui=main_ui(), server=server, static_assets=data_dir)
